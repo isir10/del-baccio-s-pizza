ef3ed812a5bcb4e167302a1f7072a4415381298b
isir10 <isir.malak@moringaschool.com>
2022-03-21 17:39:13 +0300

    added click function on proceed button

--- CSS/JS/scripts.js
+++ CSS/JS/scripts.js
@@ -7,3 +7,20 @@ function Getpizza( name,size,crust,topping, total ){
   this.topping = topping;
   this.total = total;
 }
+// proceed button
+$(document).ready(function(){
+    // $("button.proceed").click(function(){
+    //   $("button.proceed").hide();
+    //   $("#information").hide();
+    //   $("div.choise").slideDown(1000);
+    // });
+    $("button.proceed").click(function(event){
+     let pname = $(".name option:selected").val();
+     let psize = $("#size option:selected").val();
+     let pcrust = $("#crust option:selected").val();
+     let ptopping = [];
+     $.each($("input[name='toppings']:checked"), function(){            
+         ptopping.push($(this).val());
+     });
+     console.log(ptopping.join(", "));
+  
