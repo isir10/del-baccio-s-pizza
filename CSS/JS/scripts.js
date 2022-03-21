64fd5da8e582999e8f6ff0e5974ebc99673d627b
isir10 <isir.malak@moringaschool.com>
2022-03-21 18:29:42 +0300

    added prevent default function and final order on click function

--- CSS/JS/scripts.js
+++ CSS/JS/scripts.js
@@ -166,4 +166,31 @@ $(document).ready(function(){
           console.log("You will pay sh. "+deliceryamount+" on delivery");
           $("#totalbill").append("Your bill plus delivery fee is: "+deliceryamount);
         });
+        / when one clicks place order button
+        $("button#final-order").click(function(event){
+          event.preventDefault();
     
+          $("#pizzatotal").hide();
+          $(".delivery").hide();
+          $("button#final-order").hide();
+          let deliceryamount= checkoutTotal+150;
+          console.log("Final Bill is: "+deliceryamount);
+          let person = $("input#name").val();
+          let phone = $("input#phone").val();
+          let location = $("input#location").val();
+    
+          if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
+      
+            $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliceryamount);
+            $("#totalbill").hide();
+            $("#finallmessage").slideDown(1200);
+          }
+          else {
+            alert("Please fill in the details for delivery!");
+            $(".delivery").show();
+            $("button#final-order").show();
+          }
+        });
+       event.preventDefault();
+      });
+    });
